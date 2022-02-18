f7c2f21edd36d5dbc668d70444c97359d86b231b
Marco Shimomoto <mashimom@gmail.com>
2022-02-18 23:40:20 +0100

    annotation deps and broken tests fixed

--- src/test/groovy/org/shimomoto/yakety/validation/util/PredicateUtilsSpec.groovy
+++ src/test/groovy/org/shimomoto/yakety/validation/util/PredicateUtilsSpec.groovy
@@ -1,6 +1,7 @@
 package org.shimomoto.yakety.validation.util
 
 import spock.lang.IgnoreRest
+import spock.lang.Shared
 import spock.lang.Specification
 
 import java.util.function.Function
@@ -8,53 +9,68 @@ import java.util.function.Predicate
 
 class PredicateUtilsSpec extends Specification {
 
-	@IgnoreRest
-	def "Derived predicate works"() {
-		given:
-		Function<String, Integer> fn = (String s) -> Integer.valueOf(s.length())
-		Predicate<Integer> p = (Integer i) -> i == 0
-		when:
-		def dp = PredicateUtils.derivedPredicate(p, fn)
+    class TruePredicate<T> implements Predicate<T> {
+        @Override
+        boolean test(T t) {
+            return true
+        }
+    }
 
-		then:
-		dp.test("")
-		!dp.test("abc")
-	}
+    class FalsePredicate<T> implements Predicate<T> {
+        @Override
+        boolean test(T t) {
+            return false
+        }
+    }
 
-	def "logic gates"() {
-		expect:
-		and.equals(a && b)
-		nand.equals(!(a && b))
-		or.equals(a || b)
-		nor.equals(!(a || b))
-		xor.equals((a && !b) || (!a && b))
-		xnor.equals((!a && !b) || (a && b))
+    @Shared
+    def truePred = new TruePredicate()
+    @Shared
+    def falsePred = new FalsePredicate()
 
-		where:
-		_ | a     | b     | and   | nand  | or    | nor   | xor   | xnor
-		0 | false | false | false | true  | false | true  | false | true
-		1 | false | true  | false | true  | true  | false | true  | false
-		2 | true  | false | false | true  | true  | false | true  | false
-		3 | true  | true  | true  | false | true  | false | false | true
-	}
+    def "Derived predicate works"() {
+        given:
+        Function<String, Integer> fn = (String s) -> Integer.valueOf(s.length())
+        Predicate<Integer> p = (Integer i) -> i == 0
+        when:
+        def dp = PredicateUtils.derivedPredicate(p, fn)
 
-	def "predicate logic gates"() {
-		given:
-		def p1 = { a }
-		def p2 = { b }
-		expect:
-		and.equals(PredicateUtils.and(p1, p2).test(""))
-		nand.equals(PredicateUtils.nand(p1, p2).test(""))
-		or.equals(PredicateUtils.or(p1, p2).test(""))
-		nor.equals(PredicateUtils.nor(p1, p2).test(""))
-		xor.equals(PredicateUtils.xor(p1, p2).test(""))
-		xnor.equals(PredicateUtils.xnor(p1, p2).test(""))
+        then:
+        dp.test("")
+        !dp.test("abc")
+    }
 
-		where:
-		_ | a     | b     | and   | nand  | or    | nor   | xor   | xnor
-		0 | false | false | false | true  | false | true  | false | true
-		1 | false | true  | false | true  | true  | false | true  | false
-		2 | true  | false | false | true  | true  | false | true  | false
-		3 | true  | true  | true  | false | true  | false | false | true
-	}
+    def "logic gates"() {
+        expect:
+        and == (a && b)
+        nand == (!(a && b))
+        or == (a || b)
+        nor == (!(a || b))
+        xor == ((a && !b) || (!a && b))
+        xnor == ((!a && !b) || (a && b))
+
+        where:
+        _ | a     | b     | and   | nand  | or    | nor   | xor   | xnor
+        0 | false | false | false | true  | false | true  | false | true
+        1 | false | true  | false | true  | true  | false | true  | false
+        2 | true  | false | false | true  | true  | false | true  | false
+        3 | true  | true  | true  | false | true  | false | false | true
+    }
+
+    def "predicate logic gates"() {
+        expect:
+        and == PredicateUtils.<String> and(p1, p2).test("")
+        nand == PredicateUtils.<String> nand(p1, p2).test("")
+        or == PredicateUtils.<String> or(p1, p2).test("")
+        nor == PredicateUtils.<String> nor(p1, p2).test("")
+        xor == PredicateUtils.<String> xor(p1, p2).test("")
+        xnor == PredicateUtils.<String> xnor(p1, p2).test("")
+
+        where:
+        _ | p1        | p2        | and   | nand  | or    | nor   | xor   | xnor
+        0 | (falsePred) | (falsePred) | false | true  | false | true  | false | true
+        1 | (falsePred) | (truePred)  | false | true  | true  | false | true  | false
+        2 | (truePred)  | (falsePred) | false | true  | true  | false | true  | false
+        3 | (truePred)  | (truePred)  | true  | false | true  | false | false | true
+    }
 }
